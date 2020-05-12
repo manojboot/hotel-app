cc8203818d1dec61ae07801bb5ceb6910b2efa89
mpatelia <manoj.patelia@gmail.com>
2020-05-12 15:52:17 +0530

    Commit in Branch

--- src/main/java/com/test/hotel/ApplicationSecurirtyConfig.java
+++ src/main/java/com/test/hotel/ApplicationSecurirtyConfig.java
@@ -1,9 +1,17 @@
 package com.test.hotel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +28,17 @@ public class ApplicationSecurirtyConfig extends WebSecurityConfigurerAdapter {
 					.anyRequest().authenticated()
 					.and()
 					.httpBasic();
+		}
+		
+		@Bean
+		@Override
+		public UserDetailsService userDetailsService() {
+			
+				List<UserDetails> users = new ArrayList<>();
+				users.add(User.withDefaultPasswordEncoder().username("mikefoley").password("password").roles("USER").build());
+				users.add(User.withDefaultPasswordEncoder().username("admin").password("password").roles("USER","ADMIN").build());
+				return new InMemoryUserDetailsManager(users);
+			
 		}
 }
 
